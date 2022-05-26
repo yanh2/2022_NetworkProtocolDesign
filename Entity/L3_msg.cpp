89a0708169bf7230589854d95bc361d7c3a148e6
Hayan Jo <59385635+yanh2@users.noreply.github.com>
2022-05-26 17:55:50 +0900

    L3_msg 수정

--- Entity/L3_msg.cpp
+++ Entity/L3_msg.cpp
@@ -20,20 +20,23 @@ int L3_msg_checkIfRejt(uint8_t* msg)
 {
     return (msg[L3_MSG_OFFSET_TYPE] == L3_MSG_TYPE_REJT);
 }
-uint8_t L3_msg_encodeReq(uint8_t* msg_data, uint8_t seq)
+uint8_t L3_msg_encodeReq(uint8_t* msg_data)
 {
     msg_data[L3_MSG_OFFSET_TYPE] =  L3_MSG_TYPE_REQ;
-    msg_data[L3_MSG_OFFSET_SEQ] = seq;
     msg_data[L3_MSG_OFFSET_DATA] = 1;
 
     return L3_MSG_REQSIZE; //return값 뭘로하지?
 }
 
-uint8_t L3_msg_encodeData(uint8_t* msg_data, uint8_t* data, int seq, int len)
+uint8_t L3_msg_encodeData(uint8_t* msg_data, uint8_t* data, int len)
 {
     msg_data[L3_MSG_OFFSET_TYPE] = L3_MSG_TYPE_DATA;
-    msg_data[L3_MSG_OFFSET_SEQ] = seq;
     memcpy(&msg_data[L3_MSG_OFFSET_DATA], data, len*sizeof(uint8_t));
 
     return len+L3_MSG_OFFSET_DATA; //?
+}
+
+uint8_t* L3_msg_getWord(uint8_t* msg)
+{
+    return &msg[L3_MSG_OFFSET_DATA];
 }
